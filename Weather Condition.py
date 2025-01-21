06119d709634afab98ae61ce50a2694ff12227e9
Liban Tahsin <tahsin.liban0927@gmail.com>
2025-01-21 16:42:56 +0600

    first commit

---
+++ Weather Condition.py
@@ -0,0 +1,8 @@
+def weatherCondition():
+    print("Weather in Pleasent in", s1)
+    print("Weather is same in", s2)
+    
+s1 = "Autumn"
+s2 = "Winter"
+
+weatherCondition()
---
+++ Well Wishes.py
@@ -0,0 +1,9 @@
+def wellWishes(name):
+    print(f"Hello {name}, Have a good Day! ")
+    print("I am inside of a function\n\n")
+    
+a = "Trump"
+
+wellWishes(a)
+
+print("I am Outside of a function")
