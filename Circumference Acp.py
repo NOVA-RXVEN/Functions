0e07f47cef473f28e64a6951933d131701ef8f93
Liban Tahsin <tahsin.liban0927@gmail.com>
2025-01-26 15:07:02 +0600

    ACP

---
+++ Circumference Acp.py
@@ -0,0 +1,12 @@
+r = float(input("Enter the Radius: "))
+
+def circumference():
+    return 2*pi*r
+
+def area():
+    return pi*r*r
+
+pi = 3.1416
+
+print("Circumference of Circle: ",circumference())
+print("Area of Circle: ",area())
